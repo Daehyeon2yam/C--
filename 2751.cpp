4f19091575953dc19420286396cc7eb74ca078b1
박대현 <daehyeon@bagdaehyeon-ui-MacBook-Pro.local>
2020-07-23 16:47:52 +0900

    push

---
+++ 2751.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm> //내장함수를 사용하려고 함.
+using namespace std;
+
+int main(void){
+
+    int N;
+    cin>>N;
+    int array[N];
+    for(int i=0;i<N;i++){
+        cin>>array[i];
+    }
+}
