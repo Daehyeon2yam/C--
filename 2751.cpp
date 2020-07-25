e38a08d451d439dae28b4bfc29342164155ec5eb
박대현 <daehyeon@bagdaehyeon-ui-MacBook-Pro.local>
2020-07-25 15:58:19 +0900

    commt

--- 2751.cpp
+++ 2751.cpp
@@ -2,6 +2,11 @@
 #include <algorithm> //내장함수를 사용하려고 함.
 using namespace std;
 
+void mergesort(){
+
+
+}
+
 int main(void){
 
     int N;
@@ -9,5 +14,12 @@ int main(void){
     int array[N];
     for(int i=0;i<N;i++){
         cin>>array[i];
+    } // 배열에 대한 입력
+
+
+
+    for(int i=0;i<N;i++){
+        cout<<array[i]<<endl;
     }
+    return 0;
 }
