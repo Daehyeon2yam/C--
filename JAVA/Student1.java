a67afa3fa27d45f0f211c4b38964ba7c91adceda
박대현 <daehyeon@bagdaehyeon-ui-MacBook-Pro.local>
2021-12-21 17:59:15 +0900

    12.21

---
+++ JAVA/Student1.java
@@ -0,0 +1,9 @@
+public class Student1 extends People {
+    
+    public int studentNo;
+
+    public Student1(String name, String ssn, int studentNo){
+        super(name,ssn);
+        this.studentNo=studentNo;
+    }
+}
---
+++ JAVA/StudentExampe1.java
@@ -0,0 +1,9 @@
+public class StudentExampe1 {
+    
+    public static void main(String[] args){
+        Student1 student = new Student1("홍길동","12345-12345",1);
+        System.out.println("name: "+student.name);
+        System.out.println("ssn: "+student.ssn);
+        System.out.println("studentNo: "+ student.studentNo);
+    }
+}
