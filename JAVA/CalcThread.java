7772d1b4e4abaaf1b9f99e5e8e9a0fbb7d280836
박대현 <daehyeon@bagdaehyeon-ui-MacBook-Pro.local>
2021-12-27 16:19:50 +0900

    12.27

---
+++ JAVA/CalcThread.java
@@ -0,0 +1,12 @@
+public class CalcThread extends Thread {
+    public CalcThread(String name){
+        setName(name); //스레드 이름 변경
+    }
+
+    public void run(){
+        for(int i =0;i< 2000000; i++){
+            
+        }
+        System.out.println(getName());
+    }
+}
