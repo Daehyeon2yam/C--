6af63132fedfde531555ba527a29895bcf1e3297
Daehyeon2yam <eogus1381@naver.com>
2022-01-10 17:57:37 +0900

    1.10

--- todo-react-app/src/AddTodo.js
+++ todo-react-app/src/AddTodo.js
@@ -7,15 +7,43 @@ class AddTodo extends React.Component{
         this.state = { item: { title: ""} }; //사용자의 입력을 저장할 오브젝트
     }
 
-    render(){
+    //함수 작성
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+        console.log(this.Item);
+    }
+    //함수 작성
+    onButtonClick = () => {
+        this.add(this.state.item); //add함수 사용
+        this.setState({item:{title:""}});
+    }
+    enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter'){
+            this.onButtonClick();
+        }
+    }
+
+    render(){ //함수 연결
         return (
             <Paper style={{margin: 16, padding: 16}}>
                 <Grid container>
                     <Grid xs={11} md={11} item style={{ paddingRight: 16}}>
-                        <TextField placeholder="Add Todo here" fullWidth />
+                        <TextField
+                        placeholder="Add Todo here" 
+                        fullWidth
+                        onChange={this.onInputChange}
+                        value={this.state.item.title}
+                        onKeyPress={this.enterKeyEventHandler}
+                        />
                     </Grid>
                     <Grid xs={1} md={1} item>
-                        <Button fullWidth color="secondary" variant="outlined">
+                        <Button fullWidth
+                            color="secondary" 
+                            variant="outlined"
+                            onClick={this.onButtonClick}
+                            >
                             +
                         </Button>
                     </Grid>
--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -14,10 +14,18 @@ class App extends React.Component{
       ],
     };
   }
-
+//함수 추가
+  add =(item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;// key를 위한 id 추가
+    item.done = false; //done초기화
+    thisItems.push(item); //리스트에 아이템 추가
+    this.setState({ items : thisItems}); //업데이트는 반드시 this.setState로해야함
+    console.log("items:",this.state.items);
+  }
   render(){
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin: 16}}>
+      <Paper style={{margin: 20}}>
         <List>
           {this.state.items.map((item, idx)=> (
             <Todo item={item} key={item.id} />
@@ -25,11 +33,11 @@ class App extends React.Component{
         </List>
       </Paper>
     );
-
+//함수 연결
     return(
       <div className="App">
         <Container maxWidth="md">
-          <AddTodo />
+          <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
