cb8bc9eb5bfef4fb4e33bb2d866e801054753ad3
Daehyeon2yam <eogus1381@naver.com>
2022-02-08 17:37:34 +0900

    2022.02.08

--- Spring/demo/src/main/java/com/example/demo/service/UserService.java
+++ Spring/demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,5 @@
 package com.example.demo.service;
-//사용자 데이터베이스에 저장된 사용자를 가져올떄 사용
+
 import com.example.demo.model.UserEntity;
 import com.example.demo.persistence.UserRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -10,36 +10,33 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Slf4j //로그라이브러리, 로그계의 JPA
+@Slf4j
 @Service
-
 public class UserService {
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public UserEntity create(final UserEntity userEntity) {
 		if(userEntity == null || userEntity.getEmail() == null ) {
-			throw new RuntimeException("Invalid argumnets");
+			throw new RuntimeException("Invalid arguments");
 		}
 		final String email = userEntity.getEmail();
 		if(userRepository.existsByEmail(email)) {
-			log.warn("Email already exists {}",email);
+			log.warn("Email already exists {}", email);
 			throw new RuntimeException("Email already exists");
 		}
+
 		return userRepository.save(userEntity);
 	}
-	
+
 	public UserEntity getByCredentials(final String email, final String password, final PasswordEncoder encoder) {
 		final UserEntity originalUser = userRepository.findByEmail(email);
-		
-		//matches 메서드를 이용해 패스워드가 같은지 확인한다.
-		if(originalUser != null &&
-				encoder.matches(password,
-						originalUser.getPassword())) {
+
+		// matches 메서드를 이용해 패스워드가 같은지 확인
+		if(originalUser != null && encoder.matches(password, originalUser.getPassword())) {
 			return originalUser;
 		}
-			
 		return null;
 	}
-
 }
